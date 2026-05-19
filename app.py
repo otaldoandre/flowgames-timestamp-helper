f6206b0baad09018a8add4bd85a488eeedf7ca74
otaldoandre <andrealves431@hotmail.com>
2026-05-19 11:09:48 -0300

    feat: add a generate clips preview feature

--- app.py
+++ app.py
@@ -105,14 +105,81 @@ def generate_clips():
         tela.update_idletasks()
 
         if end:
-            cmd = f'ffmpeg -i {path} -ss {start} -to {end} -c copy "{title}.mp4"'
+            cmd = f'ffmpeg -ss {start} -i "{path}" -t {end} -c copy "{title}.mp4"'
         else:
-            cmd = f'ffmpeg -i {path} -ss {start} -c copy "{title}.mp4"'
+            cmd = f'ffmpeg -ss {start} -i "{path}" -c copy "{title}.mp4"'
 
         txt_saida.insert(tk.END, f"Clipe {i + 1}: Completo - {i + 1}/{total_segments}\n")
 
         os.system(cmd)
 
+
+def generate_clips_preview():
+    # A função assume que o usuário selecionou o arquivo de live correto, onde todos timestamps contidos no arquivo
+    path = filedialog.askopenfilename(
+        title="Selecione a live full",
+        filetypes=[("MP4 files", "*.mp4")]
+    )
+    if not path:
+        messagebox.showerror("Erro", "O arquivo não foi selecionado! Tente novamente.")
+        return
+
+    segments = get_clips_segments()
+    if segments is None:
+        return
+
+    #Formata para exibir apenas o dia (DD/MM/AAAA) e o horário (HH:MM)
+    date = datetime.now().strftime("%d/%m/%Y %H:%M")
+    date = get_clean_title(date)
+
+    os.mkdir(f"preview - {date}")
+    os.chdir(f"./preview - {date}")
+
+    total_segments = len(segments)
+    for i, seg in enumerate(segments):
+        start = seg["start"]
+        end = seg["end"]
+        title = seg["title"]
+
+        percent_progress = ((i + 1) / total_segments) * 100
+        progress.config(value=percent_progress)
+        tela.update_idletasks()
+
+        # Time variables that I can work with
+        start_dt = datetime.strptime(start, "%H:%M:%S")
+        preview_duration = timedelta(seconds=5)
+
+        valid_start = (start_dt + preview_duration).strftime("%H:%M:%S")
+        print(f"Start: {start} | Valid start: {valid_start}")
+
+        if end:
+            #Start
+            cmd_start_preview = (f'ffmpeg -i {path} -ss {start} -to {valid_start}'
+                                 f' -c copy "{title}_preview_start.mp4"')
+
+            #End
+            end_dt = datetime.strptime(end, "%H:%M:%S")
+
+            valid_end = (end_dt - preview_duration).strftime("%H:%M:%S")
+            cmd_end_preview = (f'ffmpeg -i {path} -ss {valid_end} -to {end} -c copy '
+                               f'"{title}_preview_end.mp4"')
+
+        else:
+            # Possible feature: add an end preview to the last clip with ffprobe (we need to find the duration of the
+            # whole video to do that)
+            cmd_start_preview = (f'ffmpeg -i {path} -ss {start} -to {valid_start}'
+                                 f' -c copy "{title}_preview_start.mp4"')
+            cmd_end_preview = None
+
+
+        txt_saida.insert(tk.END, f"Clipe {i + 1}: Completo - {i + 1}/{total_segments}\n")
+
+        if cmd_end_preview:
+            cmd = cmd_start_preview + " && " + cmd_end_preview
+        else:
+            cmd = cmd_start_preview
+
+        os.system(cmd)
 
 # Configuração da interface
 tela = tk.Tk()
@@ -128,18 +195,24 @@ lbl_instrucao.pack(pady=10)
 txt_entrada = scrolledtext.ScrolledText(tela, width=80, height=14, bg="#FFFFFF", fg="#000000")
 txt_entrada.pack(pady=5)
 
-btn_ajustar = tk.Button(tela, text="Criar cortes", command=generate_clips, bg="#FEF500", fg="#7F14B7", font=("Industry-Black", 10, "bold"))
-btn_ajustar.pack(pady=20)
+# Frame para agrupar os botões lado a lado
+frame_botoes = tk.Frame(tela, bg="#7F14B7")
+frame_botoes.pack(pady=20) # Empacota o container verticalmente
+
+btn_criar_cortes = tk.Button(frame_botoes, text="Criar cortes", command=generate_clips, bg="#FEF500", fg="#7F14B7", font=("Industry-Black", 10, "bold"))
+btn_criar_cortes.pack(side="left", padx=15)
+
+btn_criar_previews = tk.Button(frame_botoes, text="Criar preview de cortes", command=generate_clips_preview, bg="#FEF500", fg="#7F14B7", font=("Industry-Black", 10, "bold"))
+btn_criar_previews.pack(side="left", padx=15)
 
 lbl_saida = tk.Label(tela, text="Progresso:", bg="#7F14B7", fg="#FEF500", font=("Industry-Black", 12, "bold"))
 lbl_saida.pack(pady=10)
 
-# Campo de texto para saída de timestamps ajustadas
 txt_saida = scrolledtext.ScrolledText(tela, width=80, height=10, bg="#FFFFFF", fg="#000000")
 txt_saida.pack(pady=5)
 
 progress = Progressbar(tela, orient="horizontal", length=200, maximum=100)
-progress.pack(pady=6)
+progress.pack(pady=15)
 
 # Inicia a interface
 tela.mainloop()
